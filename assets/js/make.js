13328801f751ec2e33fcd5cbf3a61ba29c3c2aec
Nitin Tyagi <nitin.tyagi121191@gmail.com>
2020-01-31 11:36:09 +0530

    first commit

--- assets/js/make.js
+++ assets/js/make.js
@@ -13,28 +13,28 @@ const canvas = document.getElementById('pizza')
 const ctx = canvas.getContext('2d')
 attachHandlers()
 
-function loadImages () {
+function loadImages() {
   $.each(toppings, function (key, val) {
     const img = new window.Image()
     img.setAttribute('crossOrigin', 'anonymous')
-    img.src = '/assets/toppings/' + val.image
+    img.src = '//pizza-luvrs-nitiniiti.s3.amazonaws.com/toppings/' + val.image
     val.img = img
   })
 }
 
-function attachHandlers () {
+function attachHandlers() {
   $('.topping').click(function () {
     $(this).toggleClass('added')
     addTopping(this.id)
   })
 }
 
-function addTopping (id) {
+function addTopping(id) {
   toppings[id].added = !toppings[id].added
   redrawPizza()
 }
 
-function redrawPizza () {
+function redrawPizza() {
   ctx.clearRect(0, 0, 400, 400)
   $.each(toppings, function () {
     if (this.added) {
@@ -43,11 +43,11 @@ function redrawPizza () {
   })
 }
 
-function getPizzaImage () {
+function getPizzaImage() {
   return canvas.toDataURL('image/png')
 }
 
-function sendPizza () {
+function sendPizza() {
   const topps = []
 
   $.map(toppings, function (topping) {
