64712188d730db9296cf7377ca71f0afbead33b7
Nitin Tyagi <nitin.tyagi121191@gmail.com>
2020-06-09 18:06:11 +0530

    changes

--- assets/js/make.js
+++ assets/js/make.js
@@ -17,7 +17,7 @@ function loadImages() {
   $.each(toppings, function (key, val) {
     const img = new window.Image()
     img.setAttribute('crossOrigin', 'anonymous')
-    img.src = '//pizza-luvrs-nitiniiti.s3.amazonaws.com/toppings/' + val.image
+    img.src = '//localhost:3000/assets/toppings/' + val.image
     val.img = img
   })
 }
